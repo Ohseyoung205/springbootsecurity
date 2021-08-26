a8bf404e35d449e70ab543c70f16e7e7c430ac46
Ohseyoung205 <Ohseyoung205@gmail.com>
2021-08-26 23:27:17 +0900

    Delete springbootjdbc directory

--- springbootjdbc/src/main/java/oh/sy/springbootjdbc/account/MongoAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package oh.sy.springbootjdbc.account;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MongoAccountRepository extends MongoRepository<MongoAccount,String> {
-
-}
